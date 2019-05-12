4915916d757a6ed00229264e7ba0e49b42ab639e
vano1205 <50025266+vano1205@users.noreply.github.com>
2019-05-13 00:51:44 +0900

    Update ThinkerbELL_presentation.js

--- ThinkerbELL_presentation.js
+++ ThinkerbELL_presentation.js
@@ -1,6 +1,8 @@
 var area1=document.getElementById("area1");
 var area2=document.getElementById("area2");
+var small=document.getElementById("small");
 var index=0;
+var mid=[];
 
   var firebaseConfig = {
     apiKey: "AIzaSyB8AwRXeTAYHOZC9lB6KY99UM45CgzxGqM",
@@ -13,48 +15,68 @@ var index=0;
   };
 
   firebase.initializeApp(firebaseConfig);
+  loadDatafromFirebase();
+  
+  function loadDatafromFirebase(){
+    //var location=window.location.href;
+    //var questionindex=location.indexOf('?');
+    //var parameter=location.substring(questionindex);
+    //return firebase.database().ref('JAKY/'+parameter).once('value', function(snapshot) {
+    return firebase.database().ref('JAKY/Autoshoes').once('value', function(snapshot) {
+        var myKey = snapshot.key;
+        var myValue = snapshot.val();
+        var keyList = Object.keys(myValue);
 
-updateKeywords();
-
-area1.onclick=function(){
-    index--;
-    updateKeywords();
+        for(var i=1;i<keyList.length;i++) {
+            var myKey = keyList[i];
+            mid.push(myValue[myKey].value);
+        }
+        console.log(mid);
+        updatekeywords(mid);
+        area1.onclick=function(){
+            if(index==-1){
+                index=0;
+            }
+            index--;
+            //updateFB(myKey,index);
+            //deleteFB();
+            //writeToFB(index);
+            console.log(mid);
+            updatekeywords(mid);
+        }
+        area2.onclick=function(){
+            if(index==-1){index=0;}
+            index++;
+            //updateFB(myKey,index);
+            //deleteFB();
+            //writeToFB(index);
+            updatekeywords(mid);
+        }
+    })
 }
 
-area2.onclick=function(){
-    index++;
-    updateKeywords();
+function updatekeywords(mid){
+    if(index==-1){
+        return;
+    }
+    var newkey=Object.keys(mid)[index];
+    var newkey2=Object.keys(mid)[index+1];
+    if(newkey==null){
+        location.href='ThinkerbELL_presentation2.html';
+            return;
+    }
+    leftbox.innerHTML=mid[newkey];
+    if(newkey2==null){
+        rightbox.innerHTML="End of document";
+        rightbox.style.backgroundColor="grey";
+        return;
+    }
+    rightbox.innerHTML=mid[newkey2];
+
 }
 
-function updateKeywords(){
-    ref=firebase.database().ref('keywords');
-    ref.orderByChild('index').equalTo(index).once("value").then(function(snapshot){
-        if(index==-1){
-            return;
-        }
-        var myValue=snapshot.val();
-        if(myValue==null){
-            location.href='ThinkerbELL_presentation2.html';
-            return;
-        }
-        var keyList = Object.keys(myValue);
-        var content=myValue[keyList[0]]["value"];
-        leftbox.innerHTML=content;
-        });        
-    ref.orderByChild('index').equalTo(index+1).once("value").then(function(snapshot){
-        var myValue=snapshot.val();
-        if(index==-1){
-            index=0;
-            return;
-        }
-        if(myValue==null){
-            rightbox.innerHTML="End of document";
-            rightbox.style.backgroundColor="grey";
-            return;
-        }
-        rightbox.style.backgroundColor='#EBA393';
-        var keyList = Object.keys(myValue);
-        var content=myValue[keyList[0]]["value"];
-        rightbox.innerHTML=content;
-        });
-    } 
+small.onclick=function(){
+    console.log('1');
+    alert('go to list page');
+    //location.href='ThinkerbELL_presentation.script';
+}
