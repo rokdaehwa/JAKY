426a0f571fe3c72fbff4e609120e01a3d15bd103
harryjo97 <50068750+harryjo97@users.noreply.github.com>
2019-05-15 16:41:27 +0900

    Update ThinkerbELL_script.js

--- ThinkerbELL_script.js
+++ ThinkerbELL_script.js
@@ -97,7 +97,7 @@ function addRow(keywords, index) {
         var text_index = Number(index) + 1;
         td3.innerHTML = "<button class = '-' onclick='deleteBtn(\"" + keywords + "\",\"" + index + "\" )' >-</button>";
         td1.innerHTML = keywords;
-        td0.innerHTML = "<button onclick='change_index(\"" + text_index + "\" )' >" + String(text_index) + "</button>";
+        td0.innerHTML = "<button class='ui blue button' style='width: 30%;  display: flex;align-items: center;justify-content: center; transform: scale(0.8);' onclick='change_index(\"" + text_index + "\" )' >" + String(text_index) + "</button>";
         td1.style.color = "black";
         td0.style.width = "10%";
         td3.style.width = "10%";
@@ -108,7 +108,7 @@ function addRow(keywords, index) {
         for (var i = index + 2; i < tableKeywords.childNodes.length - 1; i++) {
             var td0 = document.createElement("td");
             var temp_index = i;
-            td0.innerHTML = "<button onclick='change_index(\"" + temp_index + "\" )' >" + String(temp_index) + "</button>";
+            td0.innerHTML = "<button class='ui blue button' style='width: 30%;  display: flex;align-items: center;justify-content: center; transform: scale(0.8);' onclick='change_index(\"" + temp_index + "\" )' >" + String(temp_index) + "</button>";
             var tr = tableKeywords.childNodes[i];
             var td = document.createElement("td");
             td.innerHTML = "<button class = '-' onclick='deleteBtn(\"" + keywords + "\",\"" + Number(i - 1) + "\" )' >-</button>";
@@ -210,7 +210,7 @@ function deleteRow(keywords, order) {
         var td0 = document.createElement("td");
         var temp_index = Number(i) - 1;
         console.log(temp_index);
-        td0.innerHTML = "<button onclick='change_index(\"" + temp_index + "\" )' >" + temp_index + "</button>";
+        td0.innerHTML = "<button class='ui blue button' style='width: 30%;  display: flex;align-items: center;justify-content: center; transform: scale(0.8);' onclick='change_index(\"" + temp_index + "\" )' >" + String(temp_index) + "</button>";
         tr.childNodes[0].childNodes[0].remove();
         tr.childNodes[0].appendChild(td0.childNodes[0]);
     }
