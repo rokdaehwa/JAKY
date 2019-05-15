c5fc9f881c775e057e21c7981628dc8461160ddb
junstar98 <50356549+junstar98@users.noreply.github.com>
2019-05-15 12:10:58 +0900

    Update ThinkerbELL_login.js

--- ThinkerbELL_login.js
+++ ThinkerbELL_login.js
@@ -6,19 +6,23 @@ var inputPW = document.getElementById('inputPW');
 divLoading.style.display = 'none';
 btnStart.onclick = function(){
     if(inputID.value != "startupPresenter1" || inputPW.value != '******'){
-        alert("Invalid ID/PW");
+        //alert("Invalid ID/PW");
     }else{
-        divLoading.style.display = 'block';
-        location.href='ThinkerbELL_list.html'
+        //divLoading.style.display = 'block';
+        //location.href='ThinkerbELL_list.html'
     }
+    divLoading.style.display = 'block';
+    location.href='ThinkerbELL_list.html'
 }
 function Enter_Check(){
     if(event.keyCode== 13){
         if(inputID.value != "startupPresenter1" || inputPW.value != '******'){
-            alert("Invalid ID/PW");
+            //alert("Invalid ID/PW");
         }else{
-            divLoading.style.display = 'block';
-            location.href='ThinkerbELL_list.html'
+            //divLoading.style.display = 'block';
+            //location.href='ThinkerbELL_list.html'
         }
+        divLoading.style.display = 'block';
+        location.href='ThinkerbELL_list.html'
     }
 }
