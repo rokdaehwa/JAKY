e7bac83a6b21a862868f8874255f201bc22bf936
rokdaehwa <50035627+rokdaehwa@users.noreply.github.com>
2019-06-04 14:32:32 +0900

    Add files via upload

--- ThinkerbELL_list.js
+++ ThinkerbELL_list.js
@@ -1,5 +1,7 @@
 var tblList = document.getElementById('tblList');
 var divAdd = document.getElementById('divAdd');
+var btnDelete = document.getElementById('btnDelete');
+var newTd;
 
 
 var firebaseConfig = {
@@ -36,12 +38,78 @@ function addList(num, name) {
     $('#tblList').append(
         $('<tr>').append(
             $('<td>').append(num),
-            $('<td>').append(
+            $('<td>').prop('id', name).append(
                 $('<a>').prop('href', link).append(name)
             )
         )
     );
 }
+
+function deleteCol() {  
+    console.log('deleteCol');
+    if (btnDelete.getAttribute('class') == 'negative ui icon button') {
+        btnDelete.setAttribute('class', 'ui icon button');
+        btnDelete.removeChild(btnDelete.firstChild);
+        var cancel = document.createElement('i');
+        cancel.setAttribute('class', 'reply icon');
+        btnDelete.appendChild(cancel);
+
+        var tblList_tr = tblList.getElementsByTagName('tr');
+        for (var i = 1; i < tblList_tr.length; i++) {
+            newTd = document.createElement('td');
+            tblList_tr[i].appendChild(newTd);
+            add_input();
+        }
+    } else {
+        btnDelete.setAttribute('class', 'negative ui icon button');
+        btnDelete.removeChild(btnDelete.firstChild);
+        var trashcan = document.createElement('i');
+        trashcan.setAttribute('class', 'trash alternate outline icon');
+        btnDelete.appendChild(trashcan);
+
+        var tblList_tr = tblList.getElementsByTagName('tr');
+        for (var i = 1; i < tblList_tr.length; i++) {
+            var tr_td = tblList_tr[i].getElementsByTagName('td');
+            tblList_tr[i].deleteCell(tr_td.length - 1);
+        }
+    }
+}
+
+function add_input() {
+    var inp = document.createElement('button');
+    inp.setAttribute('class', 'negative ui icon button');
+    // inp.value('delete');
+    var icon = document.createElement('i');
+    icon.setAttribute('class', 'trash alternate outline icon');
+    inp.setAttribute('onclick', 'deleteRow(this)');
+    inp.appendChild(icon);
+    newTd.appendChild(inp);
+}
+
+function deleteRow(button) {
+    var table=button.parentNode.parentNode;
+    removeFromlistBox(table.childNodes[1].getAttribute('id'));
+}
+
+function removeFromlistBox(title) {
+    console.log(title);
+    firebase.database().ref('/JAKY/').once('value',function(snapshot) {
+        var myValue = snapshot.val();
+        if (myValue != null){
+            var keyList = Object.keys(myValue);
+            console.log(keyList);
+            for(var i = 0; i < keyList.length; i++){
+                var currentKey = keyList[i];
+                console.log(currentKey);
+                if(currentKey === title) {
+                    console.log('Deleted: ' + title);
+                    firebase.database().ref('/JAKY/' + currentKey).remove();
+                    window.location.reload();
+                }
+            }
+        }    
+    });
+} 
 
 $(document).ready(function() {
     readFromDatabase();     
